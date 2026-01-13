ab6435f3267f76ba9fcfbf3a98fa2497b44a75a1
Nilesh Ai Roy <nilesh.roy@nexucon.com>
2026-01-13 16:11:44 +0530

    clean code

--- count_rows.py
+++
@@ -1,9 +0,0 @@
-from docx import Document
-
-def count_rows(file_path):
-    doc = Document(file_path)
-    for i, table in enumerate(doc.tables):
-        print(f"Table {i} has {len(table.rows)} rows.")
-
-if __name__ == "__main__":
-    count_rows('test_table_output.docx')
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from cv-parsing!")
-
-
-if __name__ == "__main__":
-    main()
